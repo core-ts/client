cf91b171feadbd56b65810c293b5a0bec89a90e5
Duc Nguyen <minhduc140583@gmail.com>
2020-07-25 10:14:36 +0700

    Fix bug build MetaModel for jsonToDate

--- src/webclient/json.ts
+++ src/webclient/json.ts
@@ -54,7 +54,7 @@ export function build(model: Metadata): MetaModel {
         }
         case Type.Object: {
           if (attr.typeof) {
-            const x = this.build(attr.typeof);
+            const x = build(attr.typeof);
             x.attributeName = key;
             objectFields.push(x);
           }
@@ -62,7 +62,7 @@ export function build(model: Metadata): MetaModel {
         }
         case Type.Array: {
           if (attr.typeof) {
-            const y = this.build(attr.typeof);
+            const y = build(attr.typeof);
             y.attributeName = key;
             arrayFields.push(y);
           }
@@ -104,15 +104,15 @@ export function keys(model: Metadata): string[] {
 const _datereg = '/Date(';
 const _re = /-?\d+/;
 
-function jsonToDate(obj, fields: string[]) {
+function jsonToDate(obj: any, fields: string[]): void {
   if (!obj || !fields) {
     return obj;
   }
   if (!Array.isArray(obj)) {
     for (const field of fields) {
-      const val = obj[field];
-      if (val && !(val instanceof Date)) {
-        obj[field] = toDate(val);
+      const v = obj[field];
+      if (v && !(v instanceof Date)) {
+        obj[field] = toDate(v);
       }
     }
   }
